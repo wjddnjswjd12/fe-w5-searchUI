1b17b97bd63ecacfb2d943151976d0ddc7f9f166
Jungwon Lee <leejungwon96@gmail.com>
2021-03-09 17:47:56 +0900

    [add] setInterval function to move searchUI also changed some html structure of searchUI

--- public/js/searchUI.js
+++ public/js/searchUI.js
@@ -3,6 +3,10 @@ import { _ } from "./utils.js";
 const recomKeywordURL =
   "https://shoppinghow.kakao.com/v1.0/shophow/top/recomKeyword.json";
 
+const searchUl = _.$(".area_top__recomList");
+const liHeight = 37;
+let counter = 0;
+
 const loadDatas = (url) => {
   fetch(url)
     .then((response) => response.json())
@@ -15,15 +19,30 @@ const makeHTML = (datas) => {
   let str = "";
   const recomUl = _.$(".area_top__recomList");
   datas.forEach((data, i) => (str += `<li> ${i + 1}  ${data.keyword}</li>`));
-  recomUl.innerHTML = str;
+  recomUl.innerHTML = str + getFirstLi(datas);
 };
 
 const getFirstLi = (datas) => {
   //
-  return `<li>${datas[0]}</li>`;
+  return `<li id="firstClone"> 1  ${datas[0].keyword}</li>`;
 };
-const getLastLi = () => {
-  //
+const getLastLi = (datas) => {
+  return `<li id="lastClone"> ${datas.length}  ${
+    datas[datas.length - 1].keyword
+  }</li>`;
 };
+
+searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+
+setInterval(() => {
+  searchUl.style.transition = "transform 0.3s ease-in-out";
+  counter++;
+  searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+  if (counter === 13) {
+    searchUl.style.transition = "none";
+    counter = 0;
+    searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+  }
+}, 2000);
 
 export { loadDatas, recomKeywordURL };
