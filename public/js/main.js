21f3c32d6342cc84fa4d564b15b8ea8ef1b50e8b
Jungwon Lee <leejungwon96@gmail.com>
2021-03-10 23:01:00 +0900

    [refactor] PR review:  use async await
    margin축약표기사용
    setInterval 함수 안에 담기

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 import { _ } from "./utils.js";
 import { Slide } from "./slide.js";
 import { MoreProducts } from "./moreProducts.js";
-import { loadDatas, recomKeywordURL } from "./searchUI.js";
+import { renderSearchRecom } from "./searchUI.js";
 
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
   const getNode = (className) => document.querySelector(className);
   const slideList = getNode(".slide_list");
   const nextBtn = getNode(".next_button");
@@ -14,6 +14,18 @@ window.addEventListener("load", () => {
 
   const mBtn = new MoreProducts();
   mBtn.renderHTML();
+
+  requestJsonp("자동차", responseJsonpData);
 });
 
-loadDatas(recomKeywordURL);
+function requestJsonp(word, callback) {
+  const script = document.createElement("script");
+  script.src = `https://suggest-bar.daum.net/suggest?callback=${callback.name}&limit=10&mode=json&code=utf_in_out&q=${word}&id=shoppinghow_suggest`;
+  document.body.appendChild(script);
+}
+
+window["responseJsonpData"] = function (data) {
+  console.log(data);
+};
+
+renderSearchRecom();
--- public/js/searchUI.js
+++ public/js/searchUI.js
@@ -14,22 +14,27 @@ const searchRecBox = _.$(".area_top__search_recBox");
 const liHeight = 37;
 let counter = 0;
 
-const loadDatas = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((res) => res.list)
-    .then((result) => {
-      makeHTML(result);
-      renderRecomBox(result);
-    });
+const loadDatas = async (url) => {
+  const data = await fetch(url);
+  const dataJson = await data.json();
+  return dataJson;
+};
+
+const renderSearchRecom = async () => {
+  const datas = await loadDatas(recomKeywordURL);
+  makeHTML(datas.list);
+  renderRecomBox(datas.list);
+  startRolling();
 };
 
 const makeHTML = (datas) => {
   //Ul, Li 만드는 부분...s
-  let str = "";
+  let recomUlinnerHtml = "";
   const recomUl = _.$(".area_top__recomList");
-  datas.forEach((data, i) => (str += `<li> ${i + 1}  ${data.keyword}</li>`));
-  recomUl.innerHTML = str + getFirstLi(datas);
+  datas.forEach(
+    (data, i) => (recomUlinnerHtml += `<li> ${i + 1}  ${data.keyword}</li>`)
+  );
+  recomUl.innerHTML = recomUlinnerHtml + getFirstLi(datas);
 };
 
 const getFirstLi = (datas) => {
@@ -37,18 +42,19 @@ const getFirstLi = (datas) => {
   return `<li id="firstClone"> 1  ${datas[0].keyword}</li>`;
 };
 
-searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-
-setInterval(() => {
-  searchUl.style.transition = "transform 0.3s ease-in-out";
-  counter++;
+const startRolling = () => {
   searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-  if (counter === 13) {
-    searchUl.style.transition = "none";
-    counter = 0;
+  setInterval(() => {
+    searchUl.style.transition = "transform 0.3s ease-in-out";
+    counter++;
     searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-  }
-}, 2000);
+    if (counter === 13) {
+      searchUl.style.transition = "none";
+      counter = 0;
+      searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+    }
+  }, 2000);
+};
 
 const getRecomBoxData = (datas, start, end) => {
   //클릭하면 추천검색어 뜨게하는 function
@@ -71,10 +77,10 @@ const renderRecomBox = (datas) => {
 };
 
 ulDiv.addEventListener("click", (e) => {
-  searchUl.classList.toggle("flex");
-  searchDiv.classList.toggle("border_red");
+  searchUl.classList.remove("flex");
+  searchDiv.classList.add("border_red");
   searchInput.focus();
-  searchRecBox.classList.toggle("none");
+  searchRecBox.classList.remove("none");
   e.stopPropagation();
 });
 
@@ -89,4 +95,4 @@ searchInput.addEventListener("input", () => {
   _.$(".area_top__search_recBox").innerHTML = "";
 });
 
-export { loadDatas, recomKeywordURL };
+export { renderSearchRecom };
