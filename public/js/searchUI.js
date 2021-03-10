16e81ed6d2fb027b8ec5f9080a6277c896e64a9f
Jungwon Lee <leejungwon96@gmail.com>
2021-03-11 00:40:03 +0900

    conflict test

--- public/js/searchUI.js
+++ public/js/searchUI.js
@@ -4,16 +4,6 @@ const recomKeywordURL =
   "https://shoppinghow.kakao.com/v1.0/shophow/top/recomKeyword.json";
 
 const searchUl = _.$(".area_top__recomList");
-<<<<<<< HEAD
-const liHeight = 37;
-let counter = 0;
-
-const loadDatas = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((res) => res.list)
-    .then((result) => makeHTML(result));
-=======
 const ulDiv = _.$(".area_top__search_recRoll");
 const leftOL = _.$(".recBox_leftOL");
 const rightOL = _.$(".recBox_rightOL");
@@ -32,52 +22,22 @@ const renderSearchRecom = async () => {
   makeHTML(datas.list);
   renderRecomBox(datas.list);
   startRolling(datas.list);
->>>>>>> search-step2
 };
 
 const makeHTML = (datas) => {
   //Ul, Li 만드는 부분...s
-<<<<<<< HEAD
-  let str = "";
-  const recomUl = _.$(".area_top__recomList");
-  datas.forEach((data, i) => (str += `<li> ${i + 1}  ${data.keyword}</li>`));
-  recomUl.innerHTML = str + getFirstLi(datas);
-=======
   let recomUlinnerHtml = "";
   const recomUl = _.$(".area_top__recomList");
   datas.forEach(
     (data, i) => (recomUlinnerHtml += `<li> ${i + 1}  ${data.keyword}</li>`)
   );
   recomUl.innerHTML = recomUlinnerHtml + getFirstLi(datas);
->>>>>>> search-step2
 };
 
 const getFirstLi = (datas) => {
   //
   return `<li id="firstClone"> 1  ${datas[0].keyword}</li>`;
 };
-<<<<<<< HEAD
-const getLastLi = (datas) => {
-  return `<li id="lastClone"> ${datas.length}  ${
-    datas[datas.length - 1].keyword
-  }</li>`;
-};
-
-searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-
-setInterval(() => {
-  searchUl.style.transition = "transform 0.3s ease-in-out";
-  counter++;
-  searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-  if (counter === 13) {
-    searchUl.style.transition = "none";
-    counter = 0;
-    searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-  }
-}, 2000);
-
-export { loadDatas, recomKeywordURL };
-=======
 
 const startRolling = (datas) => {
   const liHeight = 37;
@@ -147,4 +107,3 @@ searchInput.addEventListener("input", () => {
 });
 
 export { renderSearchRecom };
->>>>>>> search-step2
