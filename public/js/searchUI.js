7c81e3d9e06c781cf544c60ccea834b4974df416
Jungwon Lee <leejungwon96@gmail.com>
2021-03-11 01:15:51 +0900

    [jenny]Search step2 (#41)
    * [first commit] 이전 html, css 저장 및 express환경 새로 구축
    
    * [add] ul in html and make li by javascript
    
    * [add] setInterval function to move searchUI
    also changed some html structure of searchUI
    
    * [add] eventListener on searchBox
    
    * [refactor] PR review:  use async await
    
    margin축약표기사용
    setInterval 함수 안에 담기
    
    * [refactor] prReview: change setInterval to setTimeout
    
    * test commit
    
    * conflict test

--- public/js/searchUI.js
+++ public/js/searchUI.js
@@ -4,45 +4,106 @@ const recomKeywordURL =
   "https://shoppinghow.kakao.com/v1.0/shophow/top/recomKeyword.json";
 
 const searchUl = _.$(".area_top__recomList");
-const liHeight = 37;
-let counter = 0;
+const ulDiv = _.$(".area_top__search_recRoll");
+const leftOL = _.$(".recBox_leftOL");
+const rightOL = _.$(".recBox_rightOL");
+const searchDiv = _.$(".area_top__search");
+const searchInput = _.$(".area_top__search_input");
+const searchRecBox = _.$(".area_top__search_recBox");
 
-const loadDatas = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((res) => res.list)
-    .then((result) => makeHTML(result));
+const loadDatas = async (url) => {
+  const data = await fetch(url);
+  const dataJson = await data.json();
+  return dataJson;
+};
+
+const renderSearchRecom = async () => {
+  const datas = await loadDatas(recomKeywordURL);
+  makeHTML(datas.list);
+  renderRecomBox(datas.list);
+  startRolling(datas.list);
 };
 
 const makeHTML = (datas) => {
   //Ul, Li 만드는 부분...s
-  let str = "";
+  let recomUlinnerHtml = "";
   const recomUl = _.$(".area_top__recomList");
-  datas.forEach((data, i) => (str += `<li> ${i + 1}  ${data.keyword}</li>`));
-  recomUl.innerHTML = str + getFirstLi(datas);
+  datas.forEach(
+    (data, i) => (recomUlinnerHtml += `<li> ${i + 1}  ${data.keyword}</li>`)
+  );
+  recomUl.innerHTML = recomUlinnerHtml + getFirstLi(datas);
 };
 
 const getFirstLi = (datas) => {
   //
   return `<li id="firstClone"> 1  ${datas[0].keyword}</li>`;
 };
-const getLastLi = (datas) => {
-  return `<li id="lastClone"> ${datas.length}  ${
-    datas[datas.length - 1].keyword
-  }</li>`;
+
+const startRolling = (datas) => {
+  const liHeight = 37;
+  let counter = 0;
+
+  const roll = () =>
+    new Promise((resolve) =>
+      setTimeout(() => {
+        searchUl.style.transition = "transform 0.3s ease-in-out";
+        counter++;
+        searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+        if (counter === 13) {
+          searchUl.style.transition = "none";
+          counter = 0;
+          searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+        }
+        resolve();
+      }, 2000)
+    );
+
+  searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+  // (1) test를 async로 감싸는 대신, for문을 async 즉시실행함수로 감싸도 된다
+  (async () => {
+    while (1) {
+      await roll();
+    }
+  })();
 };
 
-searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-
-setInterval(() => {
-  searchUl.style.transition = "transform 0.3s ease-in-out";
-  counter++;
-  searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
-  if (counter === 13) {
-    searchUl.style.transition = "none";
-    counter = 0;
-    searchUl.style.transform = `translateY(-${counter * liHeight}px)`;
+const getRecomBoxData = (datas, start, end) => {
+  //클릭하면 추천검색어 뜨게하는 function
+  let arr = [];
+  for (let i = start; i < end; i++) {
+    arr.push(datas[i].keyword);
   }
-}, 2000);
 
-export { loadDatas, recomKeywordURL };
+  return arr.reduce((acc, curr, i) => {
+    acc += `<li class="recomList_li"><span class="recomList_num">${
+      i + 1 + start
+    }</span> ${curr}</li>`;
+    return acc;
+  }, "");
+};
+
+const renderRecomBox = (datas) => {
+  leftOL.innerHTML = getRecomBoxData(datas, 0, 5);
+  rightOL.innerHTML = getRecomBoxData(datas, 5, 10);
+};
+
+ulDiv.addEventListener("click", (e) => {
+  searchUl.classList.remove("flex");
+  searchDiv.classList.add("border_red");
+  searchInput.focus();
+  searchRecBox.classList.remove("none");
+  e.stopPropagation();
+});
+
+_.$("body").addEventListener("click", (e) => {
+  searchUl.classList.add("flex");
+  searchDiv.classList.remove("border_red");
+  searchRecBox.classList.add("none");
+  searchInput.blur();
+});
+
+searchInput.addEventListener("input", () => {
+  _.$(".area_top__search_recBox").innerHTML = "";
+});
+
+export { renderSearchRecom };
